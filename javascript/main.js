41d185af96ed0f07adbefdf258c07405efaad47d
Lucas Fernandez <lucas.fernamdez.lmf11@gmail.com>
2021-09-24 11:38:48 -0300

    carousel terminado, agregue indicadores y movimientos con JS

--- javascript/main.js
+++ javascript/main.js
@@ -7,8 +7,53 @@ const flechaDerecha =document.getElementById('flecha-der');
 // Event listener para flecha derecha
 flechaDerecha.addEventListener('click', () => {
     fila.scrollLeft += fila.offsetWidth;
+
+    const idicadorActivo = document.querySelector('.indicadores .activo');
+    if (idicadorActivo.nextSibling) {
+        idicadorActivo.nextSibling.classList.add('activo');
+        idicadorActivo.classList.remove('activo')
+    }
+
 });
 // Event listener para flecha izquierda
 flechaIsquierda.addEventListener('click', () => {
-    fila.scrollLeft += fila.offsetWidth;
+    fila.scrollLeft -= fila.offsetWidth;
+
+    const idicadorActivo = document.querySelector('.indicadores .activo');
+    if (idicadorActivo.previousSibling) {
+        idicadorActivo.previousSibling.classList.add('activo');
+        idicadorActivo.classList.remove('activo')
+    }
+
+
+});
+
+//paginacion
+const numeroPaginas = peliculas.length / 5;
+for(let i = 0; i < numeroPaginas; i++){
+    const indicador = document.createElement('button');
+
+    if(i === 0){
+        indicador.classList.add('activo');
+    }
+
+    document.querySelector('.indicadores') .appendChild(indicador);
+    indicador.addEventListener('click', (e) => {
+        fila.scrollLeft = i * fila.offsetWidth;
+        document.querySelector('.indicadores .activo').classList.remove('activo');
+        e.target.classList.add('activo');
+    })
+}
+//hover de peliculas
+peliculas.forEach((pelicula)=>{
+    pelicula.addEventListener('mouseenter', (e) =>{
+        const elemento = e.currentTarget;
+        setTimeout(()=> {
+            peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+            elemento.classList.add('hover');
+        },300);
+    });
+});
+fila.addEventListener('mouseleave', ()=> {
+    peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
 });
